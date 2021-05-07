3d6e4551d5415b8866668df0030e5936e2ee9a41
payal-sancheti <83813590+payal-sancheti@users.noreply.github.com>
2021-05-07 14:50:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(200, 200);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(200, 200);
+    video.hide(); 
+    classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+}
+function modelLoaded() {
+    console.log('Model is Loaded');
+  }
+  function draw() {
+    image(video, 0, 0, 200, 200);
+    classifier.classify(video , gotResult);
+  }
+  function gotResult(error, results) {
+    if (error) {
+       console.error(error);
+    }
+    else {
+       console.log(results);
+       document.getElementById("result_object_name").innerHTML=results[0].label;
+       document.getElementById("result_accuracy_name").innerHTML=results[0].confidence.toFixed(2);
+    }
+ }
